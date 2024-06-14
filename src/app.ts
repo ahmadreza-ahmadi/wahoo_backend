e8fbc216851e97f3513e6cb1f40475e74fcecce8
Ahmadreza Ahmadi <ahmadrezaahmadi.dev@gmail.com>
2024-06-15 01:32:32 +0330

    build: configure vercel

--- src/app.ts
+++ src/app.ts
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(morgan('tiny'));
 app.use('/api', ApiRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
